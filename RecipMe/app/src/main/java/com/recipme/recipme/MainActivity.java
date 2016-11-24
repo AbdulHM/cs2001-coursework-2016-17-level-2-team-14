bc1a5183b80bb695a68730720fbb95276c391edb
Jahangir Alam <cs15jja2@my.brunel.ac.uk>
2016-11-24 18:48:45 +0000

    Profile Design V1

--- RecipMe/app/src/main/java/com/recipme/recipme/MainActivity.java
+++ RecipMe/app/src/main/java/com/recipme/recipme/MainActivity.java
@@ -1,7 +1,9 @@
 package com.recipme.recipme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToProfile(View view) {
+        Intent profileActivity = new Intent (this, ProfileActivity.class);
+        startActivity(profileActivity);
+    }
+
+
 }
