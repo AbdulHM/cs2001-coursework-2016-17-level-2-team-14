dab4c383134667b18b875b0a0d3e04a2c2c84ddd
Deepali Kerai <cs15dpk@academic.windsor>
2016-12-01 11:48:27 +0000

    Login page so far

--- RecipMe/app/src/main/java/com/recipme/recipme/Login.java
+++ RecipMe/app/src/main/java/com/recipme/recipme/Login.java
@@ -13,29 +13,27 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
-
-    Button bLogout;
-    EditText etName, etAge, etUsername;
-
+public class Login extends ActionBarActivity implements View.OnClickListener{
+    Button bLogin;
+    EditText etUsername, etPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        etName = (EditText) findViewById(R.id.etName);
-        etAge = (EditText) findViewById(R.id.etAge);
         etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
 
-        bLogout = (Button) findViewById(R.id.etName);
-        bLogout.setOnClickListener((View.OnClickListener) this);
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
     }
-
+    @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.bLogout:
-
-                startActivity(new Intent(this, Login.class));
+        switch(v.getId()) {
+            case R.id.bLogin:
+                break;
+            case R.id.tvRegisterLink:
+                startActivity(new Intent(this, Register.class));
                 break;
         }
     }
