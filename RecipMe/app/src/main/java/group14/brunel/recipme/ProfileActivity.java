7241b9e7476d142d4fa015b2fbf1ce576ec834d5
Deepali Kerai <deepalik141@gmail.com>
2017-01-26 16:29:07 +0000

    profile edits

--- RecipMe/app/src/main/java/group14/brunel/recipme/ProfileActivity.java
+++ RecipMe/app/src/main/java/group14/brunel/recipme/ProfileActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by deepa on 26/01/2017.
+ * Created by deepali on 26/01/2017.
  */
 
 public class ProfileActivity extends AppCompatActivity {
@@ -15,6 +15,10 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-
+//image button? To change the profile photo?
+    //description, text edits and save descriptions
+    //Should be Un-editable for other users
+    //Recipe buttons - default images? hook up recent recipe images/names that the
+        //user has made.
 
 }
