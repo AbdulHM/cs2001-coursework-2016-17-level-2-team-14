77b78bdfee9d50a19439c9c5de3c5c7f00eac593
Jahangir Alam <cs15jja2@my.brunel.ac.uk>
2017-02-02 13:52:18 +0000

    Working on Log out button

--- RecipMe/app/src/main/java/group14/brunel/recipme/MainMenu.java
+++ RecipMe/app/src/main/java/group14/brunel/recipme/MainMenu.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button mSignOutBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+//        mAuth = FirebaseApp.getInstance();
+
+        mSignOutBtn = (Button) findViewById(R.id.sign_out);
     }
 
     public void StartSearch (View view) {
