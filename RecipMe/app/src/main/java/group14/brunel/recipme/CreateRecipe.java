17503c7deeab66d0e0a49dc3933b8590e60e6d04
Jahangir Alam <cs15jja2@my.brunel.ac.uk>
2017-02-02 22:39:44 +0000

    In progress of creating a recipe in the database

--- RecipMe/app/src/main/java/group14/brunel/recipme/CreateRecipe.java
+++ RecipMe/app/src/main/java/group14/brunel/recipme/CreateRecipe.java
@@ -3,8 +3,12 @@ package group14.brunel.recipme;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -12,6 +16,9 @@ public class CreateRecipe extends AppCompatActivity {
 
     ArrayList<String> ingredientsList = null;
     ListView listy = null;
+
+    private Button mFirebaseBtn;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,21 @@ public class CreateRecipe extends AppCompatActivity {
         Spinner spin = (Spinner) findViewById(R.id.difficultySpin);
         ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Difficulty,android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(adapter);
+
+        mFirebaseBtn = (Button) findViewById(R.id.create_recipe_btn);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+//        mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String recipe ;
+//                mDatabase.push().setValue(recipe);
+//            }
+//        });
+
+
 
 
     }
