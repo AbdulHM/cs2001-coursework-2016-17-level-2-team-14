7917fc7a3da3ba21fe2a1240128a4f5445ca922b
Deepali Kerai <deepalik141@gmail.com>
2017-02-03 22:08:51 +0000

    Register -connected to Firebase

--- RecipMe/app/src/main/java/group14/brunel/recipme/Register.java
+++ RecipMe/app/src/main/java/group14/brunel/recipme/Register.java
@@ -3,16 +3,21 @@ package group14.brunel.recipme;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Register extends AppCompatActivity {
+import static group14.brunel.recipme.R.id.buttonSave;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    //public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
 
@@ -47,12 +52,38 @@ public class Register extends AppCompatActivity {
 
         editTextAddress = (EditText) findViewById(R.id.register_email);
         editTextName = (EditText) findViewById(R.id.register_username);
-        buttonSave = (Button) findViewById(R.id.register_button);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         //initializing views
         //(Add in the code if you want it to display the email address and what not
+        //Logout button?
+
+        buttonSave.setOnClickListener(this);
+
+    }
+
+    private void saveUserInformation(){
+
+    String name = editTextName.getText().toString().trim();
+    String add = editTextAddress.getText().toString().trim();
+
+        UserInformation usernInformation = new UserInformation(name, add);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(usernInformation);
+
+        Toast.makeText(this, "Info Saved", Toast.LENGTH_LONG).show();
+
+    }
+    public void onClick (View v){
+        if(v == buttonSave){
+            saveUserInformation();
+
+    }
+
     }
 
 }
